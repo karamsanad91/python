daf017af9e0d7519e024072b511e660eecbe3ab4
karamsanad91 <zainzain2011333@gmail.com>
2018-11-05 01:55:43 +0300

    mm

--- python/week4-2.py
+++ python/week4-2.py
@@ -1,7 +1,7 @@
 print("numbers")
 num=9
 input('guss the number:')
-while int(input('guss the numbere:')) = num:
+while int(input('guss the numbere:'))= num:
     if 1 < int(input('guss the number:')) >10:
     continue
 else:
