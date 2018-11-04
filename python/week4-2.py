db149a201d236973e0322b0c1da3abb52f70dae5
karamsanad91 <zainzain2011333@gmail.com>
2018-11-04 04:34:48 +0300

    week4-2

---
+++ python/week4-2.py
@@ -0,0 +1,8 @@
+print("numbers")
+num=9
+input('guss the number:')
+while int(input('guss the numbere:')) = num:
+    if 1 < int(input('guss the number:')) >10:
+    continue
+else:
+    print('great you did it')
